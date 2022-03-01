a0c4421454b01c3fb2f171ae5abea962908a41a2
EmaRCB <a17003636@alumnos.uady.mx>
2022-03-01 16:55:59 -0600

    Inicio

---
+++ Ejercicios de programación/3_5_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    x = 21;
+    int x;
+    x = 42;
+    printf("%d", x);
+}
---
+++ Ejercicios de programación/3_5_2.java
@@ -0,0 +1,8 @@
+public class 3_5_3{
+    public static void main(String[] args) {
+        x = 21;
+        int x;
+        x = 42;
+        
+    }
+}
